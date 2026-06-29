138ab175cda7787fae4fecb7e241d5f1dfd555fa
Khooftman <k.hooftman@gmail.com>
2026-06-29 22:51:10 +0100

    Update inter.c

--- inter/inter.c
+++ inter/inter.c
@@ -1,25 +1,30 @@
 #include <unistd.h>
 
-int	main(int ac, char **av)
+int main    (int ac, char **av)
 {
-	int i = 0;
-    int seen[256] = {0};
+    int seen [256] = {0};
+    int i = 0;
 
     if (ac == 3)
     {
+    // Stap 1:registreren wat in s2 zit
+        while (av[2][i])
+        {
+            seen[(unsigned char)av[2][i]] = 1;
+            i++;
+        }
+    
+        // STAP 2: Loop door string 1 (av[1]) om te printen (garandeert de juiste volgorde!)
+        i = 0;
         while (av[1][i])
         {
-            int j = 0;
-            while (av[2][j] && av[2][j] != av[1][i])
-                j++;
-            if (av[2][j] && !seen[av[1][i]])
+            if (seen[(unsigned char)av[1][i]] == 1)
             {
                 write(1, &av[1][i], 1);
-                seen[av[1][i]] = 1;
+                seen[(unsigned char)av[1][i]] = 0; // Voorkom dubbel printen!
             }
             i++;
         }
-    }
     write(1, "\n", 1);
-    return (0);
+    }
 }
