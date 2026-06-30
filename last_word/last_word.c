4fff3c42c59698419e3cbbcead9c32bc6f9fc7fc
Khooftman <k.hooftman@gmail.com>
2026-06-30 19:06:55 +0100

    Refactor last_word.c for better clarity and functionality
    Refactor last_word.c to improve readability and structure.

--- last_word/last_word.c
+++ last_word/last_word.c
@@ -1,32 +1,35 @@
 #include <unistd.h>
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	if (ac == 2)
-	{
-		int i = 0;
+    int i = 0;
 
-		// 1. Sprint naar het einde
-		while (av[1][i])
-			i++;
-		
-		// 2. Skip spaties aan het einde (ga terug)
-		i--;
-		while (i >= 0 && (av[1][i] == ' ' || av[1][i] == '\t'))
-			i--;
-		
-		// 3. Loop terug naar het begin van het woord
-		while (i >= 0 && av[1][i] != ' ' && av[1][i] != '\t')
-			i--;
-		
-		// 4. Zet i op de eerste letter en print vooruit
-		i++;
-		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
-		{
-			write(1, &av[1][i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
+    if (ac == 2)
+    {
+        // 1. Loop naar het absolute einde van de string (\0)
+        while (av[1][i])
+            i++;
+
+        // 2. Loop terug over de spaties aan het einde.
+        // BEVEILIGING: i > 0 zorgt dat we NOOIT onder de 0 zakken.
+        while (i > 0 && (av[1][i - 1] == ' ' || av[1][i - 1] == '\t'))
+            i--;
+
+        // 3. Onthoud waar het woord eindigt
+        int eind = i;
+
+        // 4. Loop terug naar het begin van het woord
+        while (i > 0 && av[1][i - 1] != ' ' && av[1][i - 1] != '\t')
+            i--;
+
+        // 5. Print het woord van 'i' tot 'eind' van voor naar achteren
+        while (i < eind)
+        {
+            write(1, &av[1][i], 1);
+            i++;
+        }
+    }
+    // 6. Altijd afsluiten met een nieuwe regel
+    write(1, "\n", 1);
+    return (0);
 }
