0e08f48da29c74920de854e135b43e83c91a5804
Khooftman <k.hooftman@gmail.com>
2026-07-15 12:38:25 +0100

    Update ft_atoi_base.c

--- ft_atoi_base/ft_atoi_base.c
+++ ft_atoi_base/ft_atoi_base.c
@@ -2,53 +2,30 @@
 
 int ft_atoi_base(const char *str, int base)
 {
-    int i = 0;
     int sign = 1;
+    int d = 0;
     int result = 0;
-    int d;
-    char c;
 
-    // 1. Sla witruimtes over (Verplicht voor 42 School)
-    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-        i++;
-
-    // 2. Behandel het teken (+ of -)
-    if (str[i] == '-')
+    if(*str == '-')
     {
         sign = -1;
-        i++;
-    }
-    else if (str[i] == '+')
-    {
-        i++;
+        str++;
     }
 
-    // 3. De hoofdloop voor het omzetten
-    while (str[i])
+    while (*str)
     {
-        // Normaliseer hoofdletters naar kleine letters
-        if (str[i] >= 'A' && str[i] <= 'Z')
-            c = str[i] + 32;
+        if (*str >= '0' && *str <= '9')
+            d = *str - '0';
+        else if (*str >= 'a' && *str <= 'z')
+            d = *str - 'a' + 10;
+        else if (*str >= 'A' && *str <= 'Z')
+            d = *str - 'A' + 10;
         else
-            c = str[i];
-
-        // Bepaal de waarde van het huidige karakter
-        if (c >= '0' && c <= '9')
-            d = c - '0';
-        else if (c >= 'a' && c <= 'f')
-            d = 10 + (c - 'a');
-        else
-            break; // Ongeldig karakter voor atoi_base
-
-        // Controleer of de waarde wel binnen de meegegeven base past
+            break;
         if (d >= base)
             break;
-
-        // Berekening (Houd het resultaat hier positief)
         result = result * base + d;
-        i++;
+        str++;
     }
-
-    // 4. Pas het teken pas helemaal aan het einde toe
     return (result * sign);
 }
