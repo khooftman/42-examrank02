92b4a2ae610ae2966e3351702a5fdaea933602e4
Khooftman <k.hooftman@gmail.com>
2026-07-15 14:55:46 +0100

    Fix character range checks in ft_atoi_base

--- ft_atoi_base/ft_atoi_base.c
+++ ft_atoi_base/ft_atoi_base.c
@@ -16,9 +16,9 @@ int ft_atoi_base(const char *str, int base)
     {
         if (*str >= '0' && *str <= '9')
             d = *str - '0';
-        else if (*str >= 'a' && *str <= 'z')
+        else if (*str >= 'a' && *str <= 'f')
             d = *str - 'a' + 10;
-        else if (*str >= 'A' && *str <= 'Z')
+        else if (*str >= 'A' && *str <= 'F')
             d = *str - 'A' + 10;
         else
             break;
