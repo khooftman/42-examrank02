89a96c96af73f0bc76979edac713556f4aad3ff9
Khooftman <k.hooftman@gmail.com>
2026-07-16 11:44:00 +0100

    Add files via upload

---
+++ lcm.c
@@ -0,0 +1,14 @@
+unsigned int lcm(unsigned int a, unsigned int b)
+{
+    unsigned int n;
+
+    if (a == 0 || b == 0)
+        return (0);
+    if(a > b)
+        n = a;
+    else
+        n = b;
+    while(n % a != 0 || n % b != 0)
+        n++;
+    return(n);
+}
