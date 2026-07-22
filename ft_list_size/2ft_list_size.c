390654b9199bdbcd66c6709059602bbfbca745ac
Khooftman <k.hooftman@gmail.com>
2026-07-22 18:09:01 +0100

    Add files via upload

---
+++ ft_list_size/2ft_list_size.c
@@ -0,0 +1,17 @@
+typedef struct s_list
+{
+    struct s_list *next;
+    void *data;
+} t_list;
+
+int ft_list_size(t_list *begin_list)
+{
+    int n = 0;
+
+    while(begin_list)
+    {
+        begin_list = begin_list->next;
+        n++;
+    }
+    return(n);
+}
---
+++ ft_list_size/ft_list_size.c
@@ -0,0 +1,16 @@
+typedef strutc s_list
+{
+    struct s_list *next;
+    void           *data;
+} t_list;
+
+int ft_list_size(t_list *begin_list)
+{
+    int n = 0;
+    while (begin_list)
+    {
+        n++;
+        begin_list = begin_list->next;
+    }
+    return (n);
+}   
