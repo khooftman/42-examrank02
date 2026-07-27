7a028c2ecf035923afb99b0107c095f024ace2b9
Khooftman <k.hooftman@gmail.com>
2026-07-27 11:48:05 +0100

    Add files via upload

---
+++ sort_list/2sort_list.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+t_list  *sort_list(t_list* lst, int (*cmp)(int, int))
+{
+    t_list *head = lst;
+
+    while (lst && lst->next)
+    {
+        if(!cmp(lst->data, lst->next->data))
+        {
+            int tmp = lst->data;
+            lst->data = lst->next->data;
+            lst->next->data = tmp;
+            lst = head;
+        }
+        else
+            lst = lst->next;
+    }
+}
